c97dec393ce1977743d25bf493f07681f28c9304
Eric Lease Morgan <emorgan@nd.edu>
2020-07-04 22:41:29 +0000

    Added sbatch functionality

---
+++ bin/make-carrel.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# make-carrel.sh - given the name of a desired study carrel, make it
+
+# Eric Lease Morgan <emorgan@nd.edu>
+# (c) University of Notre Dame; distributed under a GNU Public License
+
+# July 4, 2020 - va n svg bs ybaryl perngvivgl
+
+
+# configure
+TEMPLATE='/home/emorgan/reader-trust/etc/template.slurm'
+CARRELS='/home/emorgan/reader-trust/carrels'
+SLURM='./make-carrel.slurm'
+
+# check for input
+if [[ -z $1 ]]; then
+	echo "Usage $0 <name>" >&2
+	exit
+fi
+
+# get input
+NAME=$1
+
+# make sane
+cd "$CARRELS/$NAME"
+
+# create Slurm script
+cat $TEMPLATE | sed "s/##NAME##/$NAME/" > $SLURM
+
+# run the command and done
+sbatch $SLURM
+exit
